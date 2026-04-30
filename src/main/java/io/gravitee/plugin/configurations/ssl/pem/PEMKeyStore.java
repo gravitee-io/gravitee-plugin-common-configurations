640f7ed9d7a79a6e56f6da38573db9e92ae81cfe
Wojciech Baszczyk <wojciech.baszczyk@graviteesource.com>
2026-04-30 15:14:27 +0200

    feat(ssl): expose hostnameVerificationAlgorithm and multi-cert PEM

--- src/main/java/io/gravitee/plugin/configurations/ssl/pem/PEMKeyStore.java
+++ src/main/java/io/gravitee/plugin/configurations/ssl/pem/PEMKeyStore.java
@@ -20,6 +20,7 @@ import io.gravitee.plugin.configurations.ssl.KeyStoreType;
 import io.gravitee.secrets.api.annotation.Secret;
 import io.gravitee.secrets.api.el.FieldKind;
 import java.io.Serial;
+import java.util.List;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -50,15 +51,38 @@ public class PEMKeyStore extends KeyStore {
     @Secret(FieldKind.PUBLIC_KEY)
     private String certContent;
 
+    /**
+     * Multiple certificate paths for keystores presenting more than one keypair
+     * (e.g. distinct RSA + ECDSA client identities). When populated, takes
+     * precedence over {@link #certPath} / {@link #keyPath}. Must be the same
+     * size as {@link #keyPaths}; index {@code i} of each list pairs together.
+     */
+    private List<String> certPaths;
+
+    private List<String> keyPaths;
+
     public PEMKeyStore() {
         super(KeyStoreType.PEM);
     }
 
     public PEMKeyStore(String keyPath, String keyContent, String certPath, String certContent) {
+        this(keyPath, keyContent, certPath, certContent, null, null);
+    }
+
+    public PEMKeyStore(
+        String keyPath,
+        String keyContent,
+        String certPath,
+        String certContent,
+        List<String> certPaths,
+        List<String> keyPaths
+    ) {
         super(KeyStoreType.PEM);
         this.keyPath = keyPath;
         this.keyContent = keyContent;
         this.certPath = certPath;
         this.certContent = certContent;
+        this.certPaths = certPaths;
+        this.keyPaths = keyPaths;
     }
 }
